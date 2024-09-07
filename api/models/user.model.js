a5f27351002d31756b59635ca15584471445ae13
hasan814 <h.mousavi910@gmail.com>
2024-09-08 01:02:50 +0330

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,14 @@
+import { model, models, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
