c59038dee839de7f1b5db6883a7c25a18c2ba4d6
hasan814 <h.mousavi910@gmail.com>
2024-09-08 00:53:29 +0330

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
+import mongoose from "mongoose";
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongoose Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
