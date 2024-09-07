17dd6faa82a1931a395679f5b5846e13b6ede01c
hasan814 <h.mousavi910@gmail.com>
2024-09-07 16:36:10 +0330

    Added Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-400">Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
