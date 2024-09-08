919cf25d9cc074c2b0aa57a9d94a7a9b1cbe38ec
hasan814 <h.mousavi910@gmail.com>
2024-09-08 11:07:25 +0330

    Create Add for Middleware and Function to Errror

--- api/index.js
+++ api/index.js
@@ -18,10 +18,20 @@ mongoose
 const app = express();
 app.use(express.json());
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 // Middleware to handle routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
