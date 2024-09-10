efe2f56e91ea3ba9d634faafb0c3d1819bc2edd1
hasan814 <h.mousavi910@gmail.com>
2024-09-10 19:59:15 +0330

    Added Redux Persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
 
 import App from "./App.jsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
