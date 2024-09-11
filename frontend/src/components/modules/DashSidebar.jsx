06935424c863080b83f94285912298702e8e95c2
hasan814 <h.mousavi910@gmail.com>
2024-09-11 17:01:07 +0330

    Complete Sidebar of the Dashboard

---
+++ frontend/src/components/modules/DashSidebar.jsx
@@ -0,0 +1,49 @@
+import {
+  Sidebar,
+  SidebarItem,
+  SidebarItemGroup,
+  SidebarItems,
+} from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const DashSidebar = () => {
+  // ============ Location =============
+  const location = useLocation();
+
+  // ============ State ===============
+  const [tab, setTab] = useState("");
+
+  // ============ Effect ===============
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) setTab(tabFormUrl);
+  }, [location.search]);
+
+  // ============ Rendering ===============
+  return (
+    <Sidebar className="w-full md:w-56">
+      <SidebarItems>
+        <SidebarItemGroup>
+          <Link to={"/dashboard?tab=profile"}>
+            <SidebarItem
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </SidebarItem>
+          </Link>
+          <SidebarItem icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </SidebarItem>
+        </SidebarItemGroup>
+      </SidebarItems>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import DashProfile from "../components/modules/DashProfile";
+import DashSidebar from "../components/modules/DashSidebar";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  // ============ Location =============
+  const location = useLocation();
+
+  // ============ State ===============
+  const [tab, setTab] = useState("");
+
+  // ============ Effect ===============
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) setTab(tabFormUrl);
+  }, [location.search]);
+
+  // ============ Rendering ===============
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      <div className="">{tab === "profile" && <DashProfile />}</div>
+    </div>
+  );
 };
 
 export default Dashboard;
