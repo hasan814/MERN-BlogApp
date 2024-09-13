0da75ea847ff45a7dd425b09699e8cf80a4c701e
hasan814 <h.mousavi910@gmail.com>
2024-09-13 19:40:00 +0330

    Added Call to Action to The Post Page

---
+++ frontend/src/components/modules/CallToAction.jsx
@@ -0,0 +1,31 @@
+import { Button } from "flowbite-react";
+
+const CallToAction = () => {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl">
+      <div className="flex-1 flex flex-col justify-center">
+        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
+        <p className="text-gray-500 my-2">
+          Checkout these resources with 100 JavaScript Projects.
+        </p>
+        <Button
+          className="rounded-tl-xl rounded-bl-none"
+          gradientDuoTone={"purpleToPink"}
+        >
+          Learn More
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <a
+          href="https://www.100newjsprojects.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/src/assets/JS.jpg" alt="JavaScript Projects" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { HashLoader } from "react-spinners";
 import { Button } from "flowbite-react";
+import CallToAction from "../components/modules/CallToAction";
 
 const PostPage = () => {
   // ============== State ===============
@@ -73,6 +74,9 @@ const PostPage = () => {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 };
